ec24ee93a4765eb84a79fece50251747284e736d
ausp <38692347+ausp@users.noreply.github.com>
2022-12-02 19:51:43 +0000

    feat: day 1

---
+++ 2022/1/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func star1(scanner *bufio.Scanner) int {
+
+	scanner.Split(bufio.ScanLines)
+
+	bestElf := 0
+
+	currentElf := 0
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			if currentElf > bestElf {
+				bestElf = currentElf
+			}
+			currentElf = 0
+		} else {
+			pineCones, err := strconv.Atoi(scanner.Text())
+			check(err)
+			currentElf += pineCones
+		}
+	}
+
+	return bestElf
+}
+
+func star2(byteData []byte, n int) int {
+
+	var intData = []int{}                     // Initialise integer slice for later use.
+	strData := string(byteData)               // Convert input bytes to string.
+	arrData := strings.Split(strData, "\n\n") // Convert single string into array of strings, each representing an elf.
+
+	for _, d := range arrData {
+		sum := 0
+		v := strings.Split(d, "\n")
+		for _, y := range v {
+			x, err := strconv.Atoi(y) // This assumes no newline at the end of the input.
+			check(err)
+			sum += x
+		}
+		intData = append(intData, sum)
+	}
+
+	// Sort by value (ascending).
+	sort.Ints(intData)
+
+	// Grab the last n values of intData - the n highest.
+	intData = intData[len(intData)-n:]
+
+	tpc := 0
+
+	for _, pc := range intData {
+		tpc += pc
+	}
+
+	return tpc
+}
+
+func main() {
+
+	f, err := os.Open("./input.txt")
+	check(err)
+
+	r := bufio.NewReader(f)
+	s := bufio.NewScanner(r)
+
+	silverStarAnswer := star1(s)
+	fmt.Printf("Result 1: %v\n", silverStarAnswer)
+
+	rf, err := os.ReadFile("./input.txt")
+	check(err)
+
+	goldStarAnswer := star2(rf, 3)
+	fmt.Printf("Result 2: %v\n", goldStarAnswer)
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# aoc
+# AoC
 Advent of Code collection
