5c0ab53282d775fd45b857d8247e4b6ec9483054
ausp <38692347+ausp@users.noreply.github.com>
2022-12-04 06:37:43 +0000

    feat: day the fourth be with you

---
+++ 2022/4/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func atoiWrap(s string) int {
+	i, err := strconv.Atoi(s)
+	check(err)
+	return i
+}
+
+func inRange(nums string, lows string, highs string) bool {
+	numi := atoiWrap(nums)
+	lowi := atoiWrap(lows)
+	highi := atoiWrap(highs)
+	if numi >= lowi && numi <= highi {
+		return true
+	} else {
+		return false
+	}
+}
+
+func star1(sa [][]string) int {
+	count := 0
+	for _, s := range sa {
+		l := strings.Split(s[0], "-")
+		r := strings.Split(s[1], "-")
+		if (atoiWrap(l[0]) <= atoiWrap(r[0]) && atoiWrap(l[1]) >= atoiWrap(r[1])) || (atoiWrap(l[0]) >= atoiWrap(r[0]) && atoiWrap(l[1]) <= atoiWrap(r[1])) {
+			// fmt.Printf("Yes: %v\n", s)
+			count++
+		} else {
+			// fmt.Printf("No: %v\n", s)
+		}
+	}
+	return count
+}
+
+func star2(sa [][]string) int {
+	count := 0
+	for _, s := range sa {
+		l := strings.Split(s[0], "-")
+		r := strings.Split(s[1], "-")
+		if inRange(l[0], r[0], r[1]) || inRange(l[1], r[0], r[1]) || inRange(r[0], l[0], l[1]) || inRange(r[1], l[0], l[1]) {
+			// fmt.Printf("Yes: %v\n", s)
+			count++
+		} else {
+			// fmt.Printf("No: %v\n", s)
+		}
+	}
+	return count
+}
+
+func main() {
+
+	f, err := os.Open("./input.txt")
+	check(err)
+
+	s, err := csv.NewReader(f).ReadAll()
+	check(err)
+
+	firstAnswer := star1(s)
+	fmt.Printf("Result 1: %v\n", firstAnswer)
+
+	secondAnswer := star2(s)
+	fmt.Printf("Result 2: %v\n", secondAnswer)
+
+}
